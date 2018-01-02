364ea0d71dcf87595571c09b4c62434879c24967
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2018-01-03 00:14:38 +0530

    Add project files.

---
+++ RadioButtonInMvc/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RadioButtonInMvc.Models;
+
+namespace RadioButtonInMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            TestRadiobutton obj=null;
+            if (TempData["MyObj"] != null)
+            {
+                obj = (TestRadiobutton)TempData["MyObj"];
+                
+            }
+
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult Change(TestRadiobutton obj)
+        {
+            TempData["MyObj"] = obj;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RadioButtonInMvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RadioButtonInMvc")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RadioButtonInMvc")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c66181f2-628e-4071-ae7c-ee94786ba752")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
